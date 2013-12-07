a607153a15d6a56dbbd76585362f7abc44bf7300
GermanSokolov <german5@ya.ru>
2013-12-07 23:10:39 +0400

    Generate Rails app --skip-bundle --skip-test-unit. Configure .gitignore and gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+App::Application.config.secret_key_base = '8e768a859c2c10fa2a5c9102ec5604be18335cd9bd1ed38e7a272c206795e8184eff83dd600b25247533356fb034f74dfbe025608ab642334387e93aad2967dd'
